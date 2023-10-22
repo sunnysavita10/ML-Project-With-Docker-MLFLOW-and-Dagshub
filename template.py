6d266ce23bb536f3355882e52dc4d0c0c81a4db8
sunnysavita10 <engsunny3365@gmail.com>
2023-10-22 11:20:45 +0530

    template file added

---
+++ template.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+
+package_name="DimondPricePrediction"
+
+list_of_files=[
+    "github/workflows/.gitkeep",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/components/__init__.py",
+    f"src/{package_name}/components/data_ingestion.py",
+    f"src/{package_name}/components/data_transformation.py",
+    f"src/{package_name}/components/model_trainer.py",
+    f"src/{package_name}/pipelines/__init__.py",
+    f"src/{package_name}/pipelines/training_pipeline.py",
+    f"src/{package_name}/pipelines/prediction_pipeline.py",
+    f"src/{package_name}/logger.py",
+    f"src/{package_name}/exception.py",
+    f"src/{package_name}/utils/__init__.py",
+    "notebooks/research.ipynb",
+    "notebooks/data/.gitkeep",
+    "requirements.txt",
+    "setup.py",
+    "init_setup.sh",
+]
+
+
+# here will create a directory
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    
+    """ how exist_ok works:if "directory" already exists, 
+    os.makedirs() will not raise an error, and it will do nothing. 
+    If "my_directory" doesn't exist, it will create the directory.
+    """
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w") as f:
+            pass
+    else:
+        print("file already exists")
+
+# here will use the file handling
+
+
