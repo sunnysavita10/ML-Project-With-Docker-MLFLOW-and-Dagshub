022e6acd27399bb7836a9e3c99301a80575e1b8b
sunnysavita10 <engsunny3365@gmail.com>
2023-10-22 12:01:32 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import find_packages,setup
+from typing import List
+
+"""HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements"""
 
 setup(
     name='DimondPricePrediction',
     version='0.0.1',
     author='sunny savita',
     author_email='sunny.savita@ineuron.ai',
+    install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
 )
