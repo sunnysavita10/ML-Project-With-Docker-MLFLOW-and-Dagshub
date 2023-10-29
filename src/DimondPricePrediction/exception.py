541700d6e3bf31048b908fd951854c2e5c593995
sunnysavita10 <engsunny3365@gmail.com>
2023-10-29 10:17:37 +0530

    exception module added

---
+++ src/DimondPricePrediction/exception.py
@@ -0,0 +1,26 @@
+import sys
+
+
+class customexception(Exception):
+    def __init__(self,error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+        
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename 
+    
+    def __str__(self):
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        self.file_name, self.lineno, str(self.error_message))
+        
+        
+    
+
+if __name__ == "__main__":
+    try:
+        
+        a=1/0
+    
+    except Exception as e:
+        raise customexception(e,sys)
+        
