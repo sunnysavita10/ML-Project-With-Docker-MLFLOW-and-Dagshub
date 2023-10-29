d346eda1adfccf8a3394ee742a18b6f3d6adb65d
sunnysavita10 <engsunny3365@gmail.com>
2023-10-29 11:43:47 +0530

    data_transformation completed

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -43,6 +43,14 @@ class DataIngestion:
             
             logging.info("data ingestion part completed")
             
+            return (
+                 
+                
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+            
+            
         except Exception as e:
            logging.info("exception during occured at data ingestion stage")
            raise customexception(e,sys)
