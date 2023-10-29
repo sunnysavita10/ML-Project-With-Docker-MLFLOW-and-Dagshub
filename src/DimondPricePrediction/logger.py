e1de46855d70a006f5550e1f98006447db92ab61
sunnysavita10 <engsunny3365@gmail.com>
2023-10-29 10:17:47 +0530

    file updated

--- src/DimondPricePrediction/logger.py
+++ src/DimondPricePrediction/logger.py
@@ -16,3 +16,7 @@ logging.basicConfig(level=logging.INFO,
                     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
                     
 )
+
+
+if __name__ == '__main__':
+    logging.info("here again i am tesitng")
