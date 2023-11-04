428c0e78edaae34ff1c2a04ca61cde0c4a59e7a3
sunnysavita10 <engsunny3365@gmail.com>
2023-11-04 10:01:51 +0530

    prediction file updated

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import os
+"""import os
 
 path="notebooks/research.ipynb"
 
@@ -7,4 +7,14 @@ dir,file=os.path.split(path)
 os.makedirs(dir,exist_ok=True)
 
 with open(path,"w") as f:
-    pass
+    pass"""
+    
+    
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData
+
+
+custdataobj=CustomData(1.52,62.2,58.0,7.27,7.33,4.55,"Premium","F","VS2")
+
+data=custdataobj.get_data_as_dataframe()
+
+print(data)
